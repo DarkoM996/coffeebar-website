e1bb7da618cd0dd4c1c792688ad884fa44429d32
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-10-26 12:34:53 +0200

    initial component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import Navbar from "./components/Navbar";
+
 const App = () => {
   return (
-    <div className="text-5xl font-bold">
-      Welcome to this React and Typescript Project
+    <div className="">
+      <Navbar />
+      <h1 className="text-5xl font-bold">
+        Welcome to this React and Typescript Project
+      </h1>
     </div>
   );
 };
