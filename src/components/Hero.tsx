866342156a459e5c9c79a00f7bbd18e2b2cfdf67
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-10-26 14:50:27 +0200

    prototyping with react

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,17 @@
+import { MdKeyboardArrowDown } from "react-icons/md";
+
 const Hero = () => {
   return (
     <main className="max-w-screen-3xl mx-auto w-full h-[calc(100vh-90px)] bg-stone-300">
-      Hero
+      <div className="flex flex-col justify-center items-center w-full h-full gap-8">
+        <h1 className="text-4xl md:text-6xl lg:text-9xl">
+          Gateway To The Best Coffee Experience
+        </h1>
+        <div>
+          <button>Swipe Down</button>
+          <MdKeyboardArrowDown className="text-2xl" />
+        </div>
+      </div>
     </main>
   );
 };
