97396b21b52bac6027ad918f18191602cf586cb1
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-10-26 19:12:08 +0200

    bg color changed

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,7 +2,7 @@ import { MdKeyboardArrowDown } from "react-icons/md";
 
 const Hero = () => {
   return (
-    <main className="max-w-screen-3xl mx-auto w-full h-[calc(100vh-82px)] bg-stone-300">
+    <main className="max-w-screen-3xl mx-auto w-full h-[calc(100vh-82px)] bg-stone-100">
       <div className="flex flex-col justify-center items-center w-full h-full gap-8 lg:gap-20">
         <div className="flex flex-col justify-center items-center">
           <div className="flex flex-row justify-center items-center">
