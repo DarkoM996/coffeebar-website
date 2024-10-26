fef1966e4870f421425a96b7bf7126d811de0745
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-10-26 14:34:30 +0200

    initial menu set up

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,25 @@
+import { MdMenu } from "react-icons/md";
+import { IoMdSearch } from "react-icons/io";
+import { MdPerson } from "react-icons/md";
+import { MdShoppingBag } from "react-icons/md";
+
 const Navbar = () => {
   return (
     <nav className="border-b border-stone-300">
       <div className="max-w-screen-3xl mx-auto w-full h-20 flex justify-between items-center px-6 py-4 lg:px-28">
-        Navbar
+        {/* TypeLogo */}
+        <div>
+          <h3 className="text-3xl">CoffeeBar</h3>
+        </div>
+        {/* Icons */}
+        <div className="flex justify-center items-center gap-2 text-xl">
+          <MdShoppingBag />
+          <MdPerson />
+          <IoMdSearch />
+          <div className="border-l pl-2 text-xl">
+            <MdMenu />
+          </div>
+        </div>
       </div>
     </nav>
   );
