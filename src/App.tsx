e3d6d4d68b1e7b91017335e5a7dcd03516149746
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-10-26 14:25:21 +0200

    initial navbar set up

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,9 @@ import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <div className="">
+    <>
       <Navbar />
-      <h1 className="text-9xl font-serif">
-        Welcome to this React and Typescript Project
-      </h1>
-    </div>
+    </>
   );
 };
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,11 @@
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="border-b border-stone-300">
+      <div className="max-w-screen-3xl mx-auto w-full h-20 flex justify-between items-center px-6 py-4 lg:px-28">
+        Navbar
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
