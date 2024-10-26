e72a0c9ae380a7e0f425918c7b703373fa868638
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-10-26 14:52:58 +0200

    latest

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,8 +5,9 @@ const Hero = () => {
     <main className="max-w-screen-3xl mx-auto w-full h-[calc(100vh-90px)] bg-stone-300">
       <div className="flex flex-col justify-center items-center w-full h-full gap-8">
         <h1 className="text-4xl md:text-6xl lg:text-9xl">
-          Gateway To The Best Coffee Experience
+          Gateway To The Best
         </h1>
+        <h1 className="text-4xl md:text-6xl lg:text-9xl"> Coffee Experience</h1>
         <div className="flex flex-col justify-center items-center gap-1">
           <button>Swipe Down</button>
           <MdKeyboardArrowDown className="text-2xl" />
