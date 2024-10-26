279108aa22849f5601363a5e2c73eaa90635f324
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-10-26 14:56:56 +0200

    latest

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,7 +2,7 @@ import { MdKeyboardArrowDown } from "react-icons/md";
 
 const Hero = () => {
   return (
-    <main className="max-w-screen-3xl mx-auto w-full h-[calc(100vh-90px)] bg-stone-300">
+    <main className="max-w-screen-3xl mx-auto w-full h-[calc(100vh-82px)] bg-stone-300">
       <div className="flex flex-col justify-center items-center w-full h-full gap-8">
         <h1 className="text-4xl md:text-6xl lg:text-9xl">
           Gateway To The Best
