7c7c766f85fa9e7fb4e09b6d33b42076cb81952e
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-10-26 14:51:48 +0200

    latest

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,7 +7,7 @@ const Hero = () => {
         <h1 className="text-4xl md:text-6xl lg:text-9xl">
           Gateway To The Best Coffee Experience
         </h1>
-        <div>
+        <div className="flex flex-col justify-center items-center gap-1">
           <button>Swipe Down</button>
           <MdKeyboardArrowDown className="text-2xl" />
         </div>
