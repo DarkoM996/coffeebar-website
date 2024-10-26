9ddf08e6a5526d96b1524f52cefaae9d0399e541
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-10-26 15:18:54 +0200

    latest

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,13 +3,17 @@ import { MdKeyboardArrowDown } from "react-icons/md";
 const Hero = () => {
   return (
     <main className="max-w-screen-3xl mx-auto w-full h-[calc(100vh-82px)] bg-stone-300">
-      <div className="flex flex-col justify-center items-center w-full h-full gap-8">
-        <h1 className="text-4xl md:text-6xl lg:text-9xl">
-          Gateway To The Best
-        </h1>
-        <h1 className="text-4xl md:text-6xl lg:text-9xl"> Coffee Experience</h1>
-        <div className="flex flex-col justify-center items-center gap-1">
-          <button>Swipe Down</button>
+      <div className="flex flex-col justify-center items-center w-full h-full gap-8 lg:gap-20">
+        <div className="flex flex-col justify-center items-center">
+          <div className="flex flex-row justify-center items-center">
+            <h1 className="text-4xl md:text-6xl lg:text-9xl text-center px-6 lg:px-28">
+              Gateway To The Best Coffee Experience
+            </h1>
+          </div>
+          {/* <h1 className="text-4xl md:text-6xl lg:text-9xl"> </h1> */}
+        </div>
+        <div className="flex flex-col justify-center items-center gap-2">
+          <button className="text-xl ">Swipe Down</button>
           <MdKeyboardArrowDown className="text-2xl" />
         </div>
       </div>
