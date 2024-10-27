34f648fbe8cac452f19a48f43beb0bdb7deb9891
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-10-27 22:33:18 +0100

    colors work now

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,24 +8,23 @@ export default {
       },
       fontFamily: {
         serif: ["Italiana", "serif"],
-        color: {
-          primary_backgroundLighterColor: {
-            50: "#FAF8F5",
-            100: "#F2EFEB",
-            200: "#E8E4DF",
-            300: "#DFD9D3",
-            400: "#D0C8BF",
-            500: "#CBC0B4",
-          },
-          // To be redefined
-          primary_backgroundDarkerColor: {
-            600: "#805A4A",
-            700: "#67493C",
-            800: "#4D372D",
-            850: "#211712",
-            900: "#1A120F",
-            950: "#271C17",
-          },
+      },
+      colors: {
+        primary_backgroundLighterColor: {
+          50: "#FAF8F5",
+          100: "#F2EFEB",
+          200: "#E8E4DF",
+          300: "#DFD9D3",
+          400: "#D0C8BF",
+          500: "#CBC0B4",
+        },
+        primary_backgroundDarkerColor: {
+          600: "#805A4A",
+          700: "#67493C",
+          800: "#4D372D",
+          850: "#211712",
+          900: "#1A120F",
+          950: "#271C17",
         },
       },
     },
