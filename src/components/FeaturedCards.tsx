ca08142f86da238a50f0dec90d36f2adec247a91
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-10-27 11:14:12 +0100

    testing a layout + new component

---
+++ src/components/FeaturedCards.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const FeaturedCards = () => {
+  return (
+    <section className="max-w-screen-3xl mx-auto w-full h-screen px-6 py-4 lg:px-28">
+      FeaturedCards
+    </section>
+  );
+};
+
+export default FeaturedCards;
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,20 +1,35 @@
-import { MdKeyboardArrowDown } from "react-icons/md";
+// import { MdKeyboardArrowDown } from "react-icons/md";
+import image1 from "../images/image1.jpg";
 
 const Hero = () => {
   return (
     <main className="max-w-screen-3xl mx-auto w-full h-[calc(100vh-82px)] bg-stone-100">
       <div className="flex flex-col justify-center items-center w-full h-full gap-8 lg:gap-20">
         <div className="flex flex-col justify-center items-center">
-          <div className="flex flex-row justify-center items-center">
-            <h1 className="text-6xl lg:text-9xl text-center px-6 lg:px-28">
-              Gateway To The Best Coffee Experience
-            </h1>
+          <div className="flex flex-col md:flex-row justify-center items-center">
+            <div>
+              <h1 className="text-5xl">Gateway To The</h1>
+            </div>
+            <div>
+              <img
+                src={image1}
+                alt="image 1"
+                className="w-[200px] h-[200px] object-cover"
+              />
+            </div>
           </div>
-          {/* <h1 className="text-4xl md:text-6xl lg:text-9xl"> </h1> */}
-        </div>
-        <div className="flex flex-col justify-center items-center gap-2">
-          <button className="text-xl ">Swipe Down</button>
-          <MdKeyboardArrowDown className="text-2xl" />
+          <div className="flex flex-col md:flex-row justify-center items-center">
+            <div>
+              <img
+                src={image1}
+                alt="image 1"
+                className="w-[200px] h-[200px] object-cover"
+              />
+            </div>
+            <div>
+              <h1 className="text-5xl">Best Coffee Experience</h1>
+            </div>
+          </div>
         </div>
       </div>
     </main>
@@ -22,3 +37,10 @@ const Hero = () => {
 };
 
 export default Hero;
+
+{
+  /* <div className="flex flex-col justify-center items-center gap-2">
+  <button className="text-xl ">Swipe Down</button>
+  <MdKeyboardArrowDown className="text-2xl" />
+</div> */
+}
