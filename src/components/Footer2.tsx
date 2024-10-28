4c7ee861415cf7251e86f3717ae4c4dc0967bf1c
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-10-28 13:10:04 +0100

    more adjustments

--- src/components/Footer2.tsx
+++ src/components/Footer2.tsx
@@ -5,38 +5,58 @@ import {
   FaPinterest,
   FaTwitter,
 } from "react-icons/fa";
+import { MdFax, MdHouse, MdLocalPhone, MdMail } from "react-icons/md";
 
 const Footer2 = () => {
   return (
-    <footer className="max-w-screen-3xl mx-auto w-full  relative px-6 py-4 lg:px-28">
-      <div className="flex flex-col md:flex-row justify-between items-center gap-6 w-full h-full">
+    <footer className="max-w-screen-3xl mx-auto w-full relative px-6 py-4 lg:px-28 bg-primary_backgroundDarkerColor-950">
+      <div className="flex flex-col md:flex-row justify-between items-center gap-6 w-full h-full py-6 border-b border-primary_backgroundDarkerColor-800 border-spacing-x-4">
         {/* Flex elements */}
         <div className="">
-          <h4 className="text-3xl">CoffeeBar</h4>
+          <h4 className="text-3xl text-primary_backgroundLighterColor-500">
+            CoffeeBar
+          </h4>
         </div>
         <div className="flex items-center gap-2">
-          <a href="/" className="text-2xl mr-2">
+          <a
+            href="/"
+            className="text-2xl mr-2 text-primary_backgroundLighterColor-500"
+          >
             <FaInstagram />
           </a>
-          <a href="/" className="text-2xl mr-2">
+          <a
+            href="/"
+            className="text-2xl mr-2 text-primary_backgroundLighterColor-500"
+          >
             <FaFacebook />
           </a>
-          <a href="/" className="text-2xl mr-2">
+          <a
+            href="/"
+            className="text-2xl mr-2 text-primary_backgroundLighterColor-500"
+          >
             <FaYoutube />
           </a>
-          <a href="/" className="text-2xl mr-2">
+          <a
+            href="/"
+            className="text-2xl mr-2 text-primary_backgroundLighterColor-500"
+          >
             <FaTwitter />
           </a>
-          <a href="/" className="text-2xl mr-2">
+          <a
+            href="/"
+            className="text-2xl mr-2 text-primary_backgroundLighterColor-500"
+          >
             <FaPinterest />
           </a>
         </div>
       </div>
       {/* More items */}
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 lg:gap-16 w-full py-12">
-        <div>
-          <h4 className="text-3xl">CoffeeBar</h4>
-          <p className="text-xl">
+        <div className="flex flex-col justify-start items-start gap-4">
+          <h4 className="text-3xl text-primary_backgroundLighterColor-500">
+            CoffeeBar
+          </h4>
+          <p className="text-xl text-primary_backgroundLighterColor-500">
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi
             reprehenderit, voluptas nemo repudiandae eius ipsam mollitia,
             accusamus nobis debitis ipsum velit cum libero quia ea. Sequi
@@ -44,28 +64,68 @@ const Footer2 = () => {
           </p>
         </div>
         {/* Featured Section */}
-        <div>
-          <h4 className="text-3xl">Featured Coffees</h4>
-          <p className="text-xl">Espresso</p>
-          <p className="text-xl">Espresso</p>
-          <p className="text-xl">Espresso</p>
-          <p className="text-xl">Espresso</p>
+        <div className="flex flex-col justify-start items-start gap-4">
+          <h4 className="text-3xl text-primary_backgroundLighterColor-200">
+            Featured Coffees
+          </h4>
+          <div className="flex flex-col justify-start items-start gap-2">
+            <p className="text-xl text-primary_backgroundLighterColor-500">
+              Espresso
+            </p>
+            <p className="text-xl text-primary_backgroundLighterColor-500">
+              Espresso
+            </p>
+            <p className="text-xl text-primary_backgroundLighterColor-500">
+              Espresso
+            </p>
+            <p className="text-xl text-primary_backgroundLighterColor-500">
+              Espresso
+            </p>
+          </div>
         </div>
         {/* Featured Section */}
-        <div>
-          <h4 className="text-3xl">Featured Coffees</h4>
-          <p className="text-xl">Espresso</p>
-          <p className="text-xl">Espresso</p>
-          <p className="text-xl">Espresso</p>
-          <p className="text-xl">Espresso</p>
+        <div className="flex flex-col justify-start items-start gap-4">
+          <h4 className="text-3xl text-primary_backgroundLighterColor-200">
+            Featured Coffees
+          </h4>
+          <div className="flex flex-col justify-start items-start gap-2">
+            <p className="text-xl text-primary_backgroundLighterColor-400">
+              Espresso
+            </p>
+            <p className="text-xl text-primary_backgroundLighterColor-400">
+              Espresso
+            </p>
+            <p className="text-xl text-primary_backgroundLighterColor-400">
+              Espresso
+            </p>
+            <p className="text-xl text-primary_backgroundLighterColor-400">
+              Espresso
+            </p>
+          </div>
         </div>
         {/* Featured Section */}
-        <div>
-          <h4 className="text-3xl">Featured Coffees</h4>
-          <p className="text-xl">Espresso</p>
-          <p className="text-xl">Espresso</p>
-          <p className="text-xl">Espresso</p>
-          <p className="text-xl">Espresso</p>
+        <div className="flex flex-col justify-start items-start gap-4">
+          <h4 className="text-3xl text-primary_backgroundLighterColor-200">
+            Featured Coffees
+          </h4>
+          <div className="flex flex-col justify-start items-start gap-2">
+            <p className="mb-4 flex items-center justify-center md:justify-start">
+              <MdHouse className="text-2xl mr-1" />
+              <a className="text-[#bebab2]">London, UK</a>
+            </p>
+            <p className="mb-4 flex items-center justify-center md:justify-start">
+              <MdMail className="text-2xl mr-1" />
+              <a className="text-[#bebab2]">travellife@contact.com</a>
+            </p>
+            <p className="mb-4 flex items-center justify-center md:justify-start">
+              <MdLocalPhone className="text-2xl mr-1" />
+              <a className="text-[#bebab2]">+ 01 234 567 88</a>
+            </p>
+            <p className="mb-4 flex items-center justify-center md:justify-start">
+              <MdFax className="text-2xl mr-1" />
+              <a className="text-[#bebab2]">+ 01 234 567 89</a>
+            </p>
+          </div>
         </div>
       </div>
       {/* Copyright section */}
