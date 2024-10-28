dd3d82e9b9c979c63ec85f605f3c1d22e5f50a5a
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-10-28 13:18:44 +0100

    new page route added

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,14 @@ import {
 import HomePage from "./pages/HomePage";
 import MainLayout from "./layouts/MainLayout";
 import CoffeesPage from "./pages/CoffeesPage";
+import AboutPage from "./pages/AboutPage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayout />}>
       <Route index element={<HomePage />} />
       <Route path="/coffees" element={<CoffeesPage />} />
+      <Route path="/about" element={<AboutPage />} />
     </Route>
   )
 );
