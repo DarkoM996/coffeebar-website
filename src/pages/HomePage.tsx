616aeacadb647d4effc92f01b655c94991cabbae
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-10-28 11:55:21 +0100

    footer will be reworked

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,17 @@
+import CTA from "../components/CTA";
+import FeaturedCards from "../components/FeaturedCards";
 import Hero from "../components/Hero";
+import NewestCoffees from "../components/NewestCoffees";
+import Footer from "./../components/Footer";
 
 const HomePage = () => {
   return (
     <>
       <Hero />
+      <FeaturedCards />
+      <NewestCoffees />
+      <CTA />
+      <Footer />
     </>
   );
 };
