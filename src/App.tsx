1b978d6c4857a6774486ff2cc79b6d6bc9f7ec18
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-10-28 11:47:21 +0100

    latest

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,17 @@
-import FeaturedCards from "./components/FeaturedCards";
-import Hero from "./components/Hero";
-import Navbar from "./components/Navbar";
-import NewestCoffees from "./components/NewestCoffees";
-import CTA from "./components/CTA";
-import Footer from "./components/Footer";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+import HomePage from "./pages/HomePage";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route index element={<HomePage />} />)
+);
 
 const App = () => {
-  return (
-    <>
-      <Navbar />
-      <Hero />
-      <FeaturedCards />
-      <NewestCoffees />
-      <CTA />
-      <Footer />
-    </>
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;
