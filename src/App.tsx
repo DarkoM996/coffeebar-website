73c112ff7335a16300c838ea9e6b72949311f61c
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-10-28 11:57:43 +0100

    new page added for now

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,13 @@ import {
 } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import MainLayout from "./layouts/MainLayout";
+import CoffeesPage from "./pages/CoffeesPage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayout />}>
-      <Route index element={<HomePage />} />)
+      <Route index element={<HomePage />} />
+      <Route path="/coffees" element={<CoffeesPage />} />
     </Route>
   )
 );
