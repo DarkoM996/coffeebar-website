fd88ca0d2247e2fab5253e07b8d3f96c7a141903
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-10-31 00:20:50 +0100

    latest

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,14 @@
 import CTA from "../components/CTA";
 import FeaturedCards from "../components/FeaturedCards";
 import Hero from "../components/Hero";
+import Navbar from "../components/Navbar";
 import NewestCoffees from "../components/NewestCoffees";
 import Footer from "./../components/Footer";
 
 const HomePage = () => {
   return (
     <>
+      <Navbar />
       <Hero />
       <FeaturedCards />
       <NewestCoffees />
