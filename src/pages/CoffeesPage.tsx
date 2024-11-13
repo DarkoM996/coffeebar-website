707fcfa102534568cd6f9f6e4dbf603732617d95
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-11-13 23:34:06 +0100

    latest

--- src/pages/CoffeesPage.tsx
+++ src/pages/CoffeesPage.tsx
@@ -1,5 +1,9 @@
 const CoffeesPage = () => {
-  return <div>CoffeesPage</div>;
+  return (
+    <section className="max-w-screen-3xl mx-auto w-full h-screen relative px-6 py-8 lg:px-28">
+      CoffeesPage
+    </section>
+  );
 };
 
 export default CoffeesPage;
