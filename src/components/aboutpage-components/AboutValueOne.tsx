9a077eaaf4790a6fe9d4b5da6aeeb7349b7bb450
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-11-13 22:32:34 +0100

    setting up the prototype

--- src/components/aboutpage-components/AboutValueOne.tsx
+++ src/components/aboutpage-components/AboutValueOne.tsx
@@ -1,5 +1,37 @@
 const AboutValueOne = () => {
-  return <section>AboutValueOne</section>;
+  return (
+    <section className="max-w-screen-3xl mx-auto w-full h-screen relative px-6 py-8">
+      {/* Grid */}
+      <div>
+        <div>
+          <h2>Coffee Shop`s story</h2>
+        </div>
+        <div>
+          <p>
+            We are devoted to making the best cup of coffee of your choice. The
+            CoffeeShop is full of passionate coffee makers.We are devoted to
+            making the best cup of coffee of your choice. We are devoted to
+            making the best cup of coffee of your choice. The CoffeeShop is full
+            of passionate coffee makers.We are devoted to making the best cup of
+            coffee of your choice.
+          </p>
+        </div>
+        <div>
+          <p>
+            We are devoted to making the best cup of coffee of your choice. The
+            CoffeeShop is full of passionate coffee makers.We are devoted to
+            making the best cup of coffee of your choice. We are devoted to
+            making the best cup of coffee of your choice. The CoffeeShop is full
+            of passionate coffee makers.We are devoted to making the best cup of
+            coffee of your choice.
+          </p>
+        </div>
+        <div>
+          <img src="" alt="" />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default AboutValueOne;
