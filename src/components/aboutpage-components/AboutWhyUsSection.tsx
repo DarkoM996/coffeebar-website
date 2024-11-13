195d2d510d0b48413c556ca7a88c7f665fc0b1df
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-11-13 22:19:05 +0100

    design adjustment

--- src/components/aboutpage-components/AboutWhyUsSection.tsx
+++ src/components/aboutpage-components/AboutWhyUsSection.tsx
@@ -2,36 +2,44 @@ import { FaCoffee } from "react-icons/fa";
 
 const AboutWhyUsSection = () => {
   return (
-    <section className="max-w-screen-3xl mx-auto w-full h-screen relative px-6 py-8 lg:px-24">
-      <div>
-        <div>
-          <h2>Why Us?</h2>
+    <section className="max-w-screen-3xl mx-auto w-full h-screen relative px-6 py-8 lg:px-16 lg:py-24">
+      <div className="flex flex-col justify-center items-center gap-6 md:gap-12 lg:gap-20">
+        <div className="py-8">
+          <h2 className="text-3xl md:text-4xl lg:text-6xl">Why Us?</h2>
         </div>
         {/* Container with icons and text */}
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 lg:gap-8 lg:px-48">
+        <div className="flex flex-col md:flex-row justify-center items-start w-full h-full gap-6 md:gap-8 lg:gap-16 lg:px-28">
           {/* Stuff #1 */}
-          <div>
-            <div className="w-[32px] h-[32px] bg-stone-300 flex flex-col justify-center items-center text-2xl">
-              <FaCoffee />
+          <div className="flex flex-row justify-center items-start gap-4 md:gap-6 lg:gap-10">
+            <div className=" bg-stone-300 rounded-full flex flex-col justify-center items-start text-2xl p-4">
+              <FaCoffee className="text-4xl text-black" />
             </div>
-            <h4>Skilled barristas</h4>
-            <p>Dedication towards a high quality of making coffee</p>
+            <div className="flex flex-col justify-center items-start gap-4">
+              <h4 className="text-2xl md:text-4xl w-full">Skilled barristas</h4>
+              <p className="lg:w-3/4">
+                Dedication towards a high quality of making coffee
+              </p>
+            </div>
           </div>
           {/* Stuff #1 */}
-          <div>
-            <div className="w-[32px] h-[32px] bg-stone-300 flex flex-col justify-center items-center text-2xl">
-              <FaCoffee />
+          <div className="flex flex-row justify-center items-center gap-4 md:gap-6 lg:gap-10">
+            <div className=" bg-stone-300 rounded-full flex flex-col justify-center items-center text-2xl p-4">
+              <FaCoffee className="text-4xl text-black" />
             </div>
-            <h4>Skilled barristas</h4>
-            <p>Dedication towards a high quality of making coffee</p>
+            <div className="flex flex-col justify-center items-start gap-2">
+              <h4 className="text-2xl md:text-4xl w-full">Skilled barristas</h4>
+              <p>Join a movement of passionate coffee drinkers</p>
+            </div>
           </div>
           {/* Stuff #1 */}
-          <div>
-            <div className="w-[32px] h-[32px] bg-stone-300 flex flex-col justify-center items-center text-2xl">
-              <FaCoffee />
+          <div className="flex flex-row justify-center items-center gap-4 md:gap-6 lg:gap-10">
+            <div className=" bg-stone-300 rounded-full flex flex-col justify-center items-center text-2xl p-4">
+              <FaCoffee className="text-4xl text-black" />
             </div>
-            <h4>Skilled barristas</h4>
-            <p>Dedication towards a high quality of making coffee</p>
+            <div className="flex flex-col justify-center items-start gap-2">
+              <h4 className="text-2xl md:text-4xl w-full">Skilled barristas</h4>
+              <p>We deliver your coffee to your comfort home</p>
+            </div>
           </div>
         </div>
       </div>
