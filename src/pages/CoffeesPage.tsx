6d49a2183760668cd6017db8c60d013e30261c4a
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-11-26 23:45:46 +0100

    latest

--- src/pages/CoffeesPage.tsx
+++ src/pages/CoffeesPage.tsx
@@ -1,7 +1,12 @@
 const CoffeesPage = () => {
   return (
     <section className="max-w-screen-3xl mx-auto w-full h-screen relative px-6 py-8 lg:px-28">
-      CoffeesPage
+      <h1>Available Coffees</h1>
+      {/* Grid of products */}
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+        {/* Product 1 */}
+        <div>asdasd</div>
+      </div>
     </section>
   );
 };
