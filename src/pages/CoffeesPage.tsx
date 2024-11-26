bee35a313819409b5ce1829702d8ecc6133976e8
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-11-27 00:11:44 +0100

    latest

--- src/pages/CoffeesPage.tsx
+++ src/pages/CoffeesPage.tsx
@@ -1,7 +1,9 @@
 const CoffeesPage = () => {
   return (
     <section className="max-w-screen-3xl mx-auto w-full h-screen relative px-6 py-8 lg:px-28">
-      <h1>Available Coffees</h1>
+      <h1 className="text-4xl md:text-5xl lg:text-7xl lg:py-12">
+        Available Coffees
+      </h1>
       {/* Grid of products */}
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
         {/* Product 1 */}
