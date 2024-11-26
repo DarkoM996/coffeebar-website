5b26995d62a1c2462064f33bd2456a7203c637d4
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-11-26 23:57:11 +0100

    initial design

--- src/pages/Error404.tsx
+++ src/pages/Error404.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+import image from "../images/error404image.jpg";
 
 const Error404 = () => {
-  return <div>Error404</div>;
+  return (
+    <section className="max-w-screen-3xl mx-auto w-full h-[calc(100vh-81px)] relative">
+      <div className="relative h-full">
+        <img
+          src={image}
+          alt="error 404 image"
+          className="w-full h-full object-cover"
+        />
+      </div>
+    </section>
+  );
 };
 
 export default Error404;
