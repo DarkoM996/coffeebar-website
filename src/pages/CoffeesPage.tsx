be948a267b075df2f0410b4b65cdad9c810baadf
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-12-01 17:31:17 +0100

    sorting out card

--- src/pages/CoffeesPage.tsx
+++ src/pages/CoffeesPage.tsx
@@ -1,13 +1,24 @@
+import image from "../images/image1.jpg";
+
 const CoffeesPage = () => {
   return (
     <section className="max-w-screen-3xl mx-auto w-full h-screen relative px-6 py-8 lg:px-28">
-      <h1 className="text-4xl md:text-5xl lg:text-7xl lg:py-12">
+      <h1 className="text-4xl md:text-5xl lg:text-7xl pb-12 lg:py-12">
         Available Coffees
       </h1>
       {/* Grid of products */}
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
         {/* Product 1 */}
-        <div>asdasd</div>
+        <div className="grid grid-cols-1 place-content-start gap-10">
+          <div className="relative w-full h-[350px]">
+            <img
+              src={image}
+              alt="coffee image"
+              className="w-full h-full object-cover rounded-tr-[250px] rounded-bl-[250px]"
+            />
+          </div>
+          <h4 className="text-xl md:text-2xl">Coffee Name</h4>
+        </div>
       </div>
     </section>
   );
